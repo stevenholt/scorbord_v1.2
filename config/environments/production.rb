998173d353717c24ebd7f148b59da425438562fc
Steven Holt <steven@scorbord.com>
2015-02-13 08:16:16 -0500

    fix Mandrill config action_mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'polar-fjord-5660.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :address    => "smtp.mandrillapp.com",
     :port       => 587, #ports 25 and 2525 are also supported with STARTTLS

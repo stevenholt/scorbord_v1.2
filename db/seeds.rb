b1f774a69ccbd5edc100460d55bf217bb6baa2e4
Steven Holt <steven@scorbord.com>
2015-02-13 06:56:13 -0500

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(	name: 		"Josh",
 				email: 		"josh@scorbord.com",
 				password: 				"hard24get",
 				password_confirmation: 	"hard24get",
-				admin: true)
+				admin: true,
+				activated: true,
+				activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(	name: 		"Josh",
 	User.create!(	name: 		name,
 					email: 		email,
 					password: 				password,
-					password_confirmation: 	password)
+					password_confirmation: 	password,
+					activated: 		true,
+					activated_at: 	Time.zone.now)
 end
